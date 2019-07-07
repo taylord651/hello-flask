570c064d36f5c8b5fa8c18e43d2ea6cfc362cf97
Domonique Taylor <taylord651@gmail.com>
2019-07-07 13:45:38 -0500

    Jinja2 for loops and if statements in html to create a todo list

--- main.py
+++ main.py
@@ -76,4 +76,16 @@ def valid_time():
     time = request.args.get('time')
     return '<h1>You submitted {0}. Thanks for submitting a valid time!</h1>'.format(time)
 
+tasks = []
+
+@app.route('/todos', methods=["GET", "POST"])
+def todos():
+
+    if request.method == "POST":
+        task = request.form['task']
+        tasks.append(task)
+
+    template = jinja_env.get_template('todos.html')
+    return template.render(tasks=tasks)
+
 app.run()
